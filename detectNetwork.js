95f552f988a0f5e7bdf27786f78e2381e3d6c815
ttreit <14933277+ttreit@users.noreply.github.com>
2020-01-08 15:38:03 -0800

    Create initial functions to test card number
    Write main function and two supporting functions. One checks
    card length and the other checks card prefixes.

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,3 +1,5 @@
+
+
 // Given a credit card number, this function should return a string with the 
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
@@ -7,12 +9,52 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
+  
+  var detectNetwork = function(cardNumber) {
+    var network = checkLength(cardNumber);
+    if (network === undefined) {
+      network = checkPrefix(cardNumber)
+      return network;
+    } else {
+      return network;
+    }
+  };
 
+  function checkLength(cardNumber) {
+    switch (cardNumber.length) {
+      case 14:
+        return 'Diner\'s Club';
+        break;
+      case 15:
+        return 'American Express';
+        break;
+      default:
+        return undefined;
+        break;
+    }
 
+  }
+
+  function checkPrefix(cardNumber) {
+    var prefix = cardNumber.slice(0, 2);
+    switch (prefix) {
+      case '38':
+        return 'Diner\'s Club';
+        break;
+      case '34':
+        return 'American Express';
+        break;
+      default:
+        return 'Unknown Bank';
+        break;
+    }
+    
+  }
+  
+// var test = detectNetwork('445602354');
+// console.log(test);
+  
